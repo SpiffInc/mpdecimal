f5a5dc717545026765c09774f5432734629ba65d
mmmries <riesmmm@gmail.com>
2021-04-17 20:25:25 -0600

    initial

---
+++ lib/mpdecimal.ex
@@ -0,0 +1,18 @@
+defmodule Mpdecimal do
+  @moduledoc """
+  Documentation for `Mpdecimal`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Mpdecimal.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/mpdecimal_test.exs
@@ -0,0 +1,8 @@
+defmodule MpdecimalTest do
+  use ExUnit.Case
+  doctest Mpdecimal
+
+  test "greets the world" do
+    assert Mpdecimal.hello() == :world
+  end
+end
