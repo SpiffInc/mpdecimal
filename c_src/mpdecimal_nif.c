322917cfbe858438cdc0ea296d0e34f19cbde095
Jonner Steck <jonner.steck@spiff.com>
2021-05-17 12:32:51 -0700

    Fix bug in creation of cstring arguments. Clean up debugging logs.

--- c_src/mpdecimal_nif.c
+++ c_src/mpdecimal_nif.c
@@ -15,10 +15,24 @@
 static mpd_context_t ctx;
 
 static int load(ErlNifEnv *env, void **priv_data, ERL_NIF_TERM load_info) {
-  mpd_init(&ctx, 28);
+	// TODO: Because the MPD context is persistent, status flags and error
+	// conditions will accumulate across multiple calls into this library. Do we
+	// need to do maintenance of any kind on these flags?
+	mpd_init(&ctx, 28);
 	ctx.traps = 0;
   return 0;
 }
+
+#ifdef NIF_DEBUG
+static void printf_data_hex(const unsigned char* data, size_t size)
+{
+	const unsigned char* end = data + size;
+	while (data < end) {
+		printf("0x%02x ", *(data++));
+	}
+	printf("\r\n");
+}
+#endif
 
 static ERL_NIF_TERM mpdecimal_power(ErlNifEnv *env, int argc, const ERL_NIF_TERM argv[])
 {
@@ -30,84 +44,101 @@ static ERL_NIF_TERM mpdecimal_power(ErlNifEnv *env, int argc, const ERL_NIF_TERM
   ErlNifBinary power;
   ERL_NIF_TERM return_value;
 
-  if(argc != 2 ||
-    !enif_is_binary(env, argv[0]) ||
-    !enif_is_binary(env, argv[1])) {
+	// Convert parameters into Erlang binaries.
+  if (argc != 2 ||
+		  !enif_inspect_binary(env, argv[0], &base) ||
+		  !enif_inspect_binary(env, argv[1], &power)) {
     return enif_make_badarg(env);
   }
 
-  #ifdef NIF_DEBUG
-  enif_fprintf(stdout, "base  (as binary): %T\r\n", argv[0]);
-  enif_fprintf(stdout, "power (as binary): %T\r\n", argv[1]);
-  #endif
+#ifdef NIF_DEBUG
+	printf("----Parameters----\r\n");
+  enif_fprintf(stdout, "base (binary): %T\r\n", argv[0]);
+	printf("  - size: %zu\r\n", base.size);
+	printf("  - encoding: ");
+	printf_data_hex(base.data, base.size);
 
-  // convert elixir string arguments into mpdecimal structs
-  enif_inspect_binary(env, argv[0], &base);
-  enif_inspect_binary(env, argv[1], &power);
+  enif_fprintf(stdout, "power (binary): %T\r\n", argv[1]);
+	printf("  - size: %zu\r\n", power.size);
+	printf("  - encoding: ");
+	printf_data_hex(power.data, power.size);
+#endif
 
-  #ifdef NIF_DEBUG
-  fprintf(stdout, "base  (as cstring): %s\r\n  - binary size: %d\r\n  - cstring strlen: %d\r\n", base.data, base.size, strlen(base.data));
-  fprintf(stdout, "power (as cstring): %s\r\n  - binary size: %d\r\n  - cstring strlen: %d\r\n", power.data, power.size, strlen(power.data));
-
-	fprintf(stdout, "base encoding:  ");
-	unsigned char* index = base.data;
-	do {
-		fprintf(stdout, "0x%02x ", *index);
-	} while (*(index++) != 0);
-	fprintf(stdout, "\r\n");
-
-	fprintf(stdout, "power encoding: ");
-	index = power.data;
-	do {
-		fprintf(stdout, "0x%02x ", *index);
-	} while (*(index++) != 0);
-	fprintf(stdout, "\r\n");
-  #endif
-
+#ifdef NIF_DEBUG
+	printf("\r\n----Converting parameters from Erlang binary to cstring----\r\n");
+#endif
 	// This relies on the property that the Erlang binary encoding uses only
 	// single-byte ASCII-compatible values.
 	// 
-	// TODO: Check whether the source of this data (Elixir Decimal library)
-	// guarantees this property.
-	char* base_cstring = (char*) malloc((base.size + 1)*sizeof(char));
-	char* power_cstring = (char*) malloc((power.size + 1)*sizeof(char));
-	strncpy(base_cstring, base.data, base.size);
-	strncpy(power_cstring, power.data, power.size);
+	// TODO: Add checks to validate that the string contains only valid ASCII
+	// characters. (i.e. Assert that the above property is true.)
+	//
+	// Allocate the same number of bytes as used by the Erlang binary, plus one
+	// for the null terminator.
+	char* base_cstring = (char*) enif_alloc((base.size + 1)*sizeof(char));
+	char* power_cstring = (char*) enif_alloc((power.size + 1)*sizeof(char));
+	strncpy(base_cstring, (char*) base.data, base.size);
+	strncpy(power_cstring, (char*) power.data, power.size);
+	// Manually add null terminator to each string.
+	base_cstring[base.size] = '\0';
+	power_cstring[power.size] = '\0';
 
+#ifdef NIF_DEBUG
+	printf("base (cstring):  %s\r\n", base_cstring);
+	printf("  - encoding: ");
+	printf_data_hex((unsigned char*) base_cstring, base.size + 1);
+	printf("power (cstring): %s\r\n", power_cstring);
+	printf("  - encoding: ");
+	printf_data_hex((unsigned char*) power_cstring, power.size + 1);
+#endif
+
+#ifdef NIF_DEBUG
+	printf("\r\n----Converting parameters from cstring to mpd----\r\n");
+	mpd_snprint_flags(status_str, MPD_MAX_FLAG_STRING, ctx.status);
+	printf("Inital mpd status: %s\r\n", status_str);
+#endif
   a = mpd_new(&ctx);
 	b = mpd_new(&ctx);
 	mpd_set_string(a, base_cstring, &ctx);
-	mpd_set_string(b, power_cstring, &ctx);
-
-  #ifdef NIF_DEBUG
+#ifdef NIF_DEBUG
+	mpd_snprint_flags(status_str, MPD_MAX_FLAG_STRING, ctx.status);
   rstring = mpd_to_sci(a, 1);
-  enif_fprintf(stdout, "mpd base: %s\r\n", rstring);
+  printf("base (mpd):   %s\r\n", rstring);
+	printf("  - status: %s\r\n", status_str);
+#endif
+	mpd_set_string(b, power_cstring, &ctx);
+#ifdef NIF_DEBUG
+	mpd_snprint_flags(status_str, MPD_MAX_FLAG_STRING, ctx.status);
   rstring = mpd_to_sci(b, 1);
-  enif_fprintf(stdout, "mpd power: %s\r\n", rstring);
-  #endif
+  printf("power (mpd):  %s\r\n", rstring);
+	printf("  - status: %s\r\n", status_str);
+#endif
 
-  // run the calculation
+#ifdef NIF_DEBUG
+	printf("\r\n----Perfoming the calculation----\r\n");
+#endif
   result = mpd_new(&ctx);
 	mpd_pow(result, a, b, &ctx);
 
-  // convert result to an elixir string
+  // Convert result to cstring.
 	rstring = mpd_to_sci(result, 1);
-  #ifdef NIF_DEBUG
-  enif_fprintf(stdout, "mpd result: %s\r\n", rstring);
-  #endif
+#ifdef NIF_DEBUG
+  printf("result (mpd): %s\r\n", rstring);
 	mpd_snprint_flags(status_str, MPD_MAX_FLAG_STRING, ctx.status);
+	printf("  - status: %s\r\n", status_str);
+#endif
   unsigned char* bin_ptr = enif_make_new_binary(env, strlen(rstring), &return_value);
-  strcpy(bin_ptr, rstring);
+  memcpy(bin_ptr, rstring, strlen(rstring));
 
-  // cleanup mpdecimal memory
+  // Clean up mpdecimal memory.
 	mpd_del(a);
 	mpd_del(b);
 	mpd_del(result);
 	mpd_free(rstring);
 
-	// Clean up locally-used memory.
-	free(base_cstring);
-	free(power_cstring);
+	// Clean up memory that was allocated locally.
+	enif_free(base_cstring);
+	enif_free(power_cstring);
 	
 	return return_value;
 }
