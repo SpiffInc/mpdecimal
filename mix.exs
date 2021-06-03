d176121265c30c2fcd3e3d3a013149709a4c4379
mmmries <riesmmm@gmail.com>
2021-06-02 20:56:42 -0700

    setup ex docs and package information for publishing

--- mix.exs
+++ mix.exs
@@ -1,6 +1,9 @@
 defmodule Mpdecimal.MixProject do
   use Mix.Project
 
+  @version "0.1.0"
+  @source_url "https://github.com/SpiffInc/mpdecimal"
+
   def project do
     [
       app: :mpdecimal,
@@ -10,7 +13,14 @@ defmodule Mpdecimal.MixProject do
       version: "0.1.0",
       elixir: "~> 1.11",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      package: package(),
+      docs: [
+        main: "readme",
+        source_ref: "v#{@version}",
+        source_url: @source_url,
+        extras: ["README.md"]
+      ]
     ]
   end
 
@@ -25,7 +35,34 @@ defmodule Mpdecimal.MixProject do
     [
       {:decimal, "~> 2.0"},
       {:elixir_make, "~> 0.6", runtime: false},
+
+      {:ex_doc, "~> 0.24.2", only: :dev},
       {:benchee, "~> 1.0", only: :dev}
+    ]
+  end
+
+  defp package do
+    [
+      description: "Provide fixed-decimal precision math as a NIF",
+      files: [
+        "lib",
+        "priv",
+        ".formatter.exs",
+        "mix.exs",
+        "README*",
+        "LICENSE*",
+        "CHANGELOG*",
+        "Makefile",
+        "c_lib",
+        "c_src"
+      ],
+      name: "mpdecimal",
+      licenses: ["MIT"],
+      links: %{"github" => @source_url},
+      maintainers: [
+        "Jonner Steck",
+        "Michael Ries"
+      ]
     ]
   end
 end
